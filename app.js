46b2902869d46d799d733e896baa9a4a893036cd
Anudle <AnuShrestha007@gmail.com>
2016-10-03 15:50:58 -0600

    updated app.js and npm installed passport packages

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+// Passport's sessions requires express-session to work
+var session = require('expresss-session')
+var passport = require('./passport')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,10 +26,24 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Configure express session
+app.use(session({
+  secret: process.env.SESSION_KEY,
+  saveUninitialized: true,
+  resave: false
+}));
+
+app.use(passport.initialize());// Mount Passport mdilldeware onto Express
+app.use(passport.session()); //Mount Passport session middleware onto Express
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index); // read posts and make comments
+app.use('/users', users); // login create delete users
+
+app.use('/posts', posts);//create new posts, edit posts, get post by id, delete posts.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
