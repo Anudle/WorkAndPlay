4635c79e11e7190f87c51e1951c1307ab23760ed
Anudle <AnuShrestha007@gmail.com>
2016-10-03 16:33:46 -0600

    added user table and passport functions

--- migrations/20161001200144_users.js
+++ migrations/20161001200144_users.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users',
+function(table) {
+  table.increments('id').primary();
+  table.string('user_name');
+  table.string('passowrd');
+  table.string('email');
+})
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('users')
 };
---
+++ model/user_query.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const knex = require('./knex_config')
+
+function addUser(userData){
+  return knex('users').insert(userData)
+}
+
+function countOfUser(userName){
+  returnknex('users').count('user_name').where('user_name', userName);
+}
+
+function findUser(userName){
+  return knex('users').where('user_name', userName).first()
+}
+
+function findUserbyName(userName){
+  return knex('users').where('users.user_name', username).first()
+}
+
+function editUserName(userID, newUserName){
+  return knex('users').where('id', userID).update({
+    user_name: newUserName.user_name
+  })
+}
+
+function editUserEmail(userID, newEmail){
+  return knex('users').where('id', userID).update({
+    email: newEmail.email
+  })
+}
+
+function editUserPassword(userID, newPassword){
+  return knex('users').where('id', userID).update({
+    password: newPassword
+  })
+}
+
+function deleteUser(userID){
+  return knex('users')
+  .where('users.id', userID).del()
+}
+
+module.exports= {
+  add: addUser,
+  count: countOfUser,
+  findUser: findUser,
+  findUserbyName: findUserbyName,
+  editName: editUserName,
+  editEmail: editUserEmail,
+  editPassword: editUserPassword,
+  deleteUser: deleteUser
+}
